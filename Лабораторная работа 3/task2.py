46d2bf6f69edda2c78436f3746d607d7c47248a4
Diniskaa <vladian65@gmail.com>
2024-11-10 00:08:41 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,9 +1,12 @@
 # TODO Напишите функцию find_common_participants
 
 def find_common_participants(str1, str2, det=','):
+    # Разбиваем первую строку по разделителю и преобразуем в множество
     set1 = set(str1.split(det))
+    # Разбиваем вторую строку по разделителю и преобразуем в множество
     set2 = set(str2.split(det))
-    common_participants = sorted(set1.intersection(set2))
+    # Находим пересечение множеств
+    common_participants = list(set1.intersection(set2))
     return common_participants
 
 participants_first_group = "Иванов|Петров|Сидоров"
